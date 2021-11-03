f6cee2040b934f9656d52c0c06366841f72d76f7
Josh Laney <joshua.laney@waeg.com>
2021-11-02 17:16:57 -0700

    PBTC-CH-1 added remaining operator overloads to FieldElement

--- bitcoin/src/Fields/include/FieldElement.hpp
+++ bitcoin/src/Fields/include/FieldElement.hpp
@@ -22,27 +22,16 @@ class FieldElement
 
     friend bool operator==(const FieldElement& lhs, const FieldElement& rhs);
     friend bool operator!=(const FieldElement& lhs, const FieldElement& rhs);
+    friend FieldElement operator+(const FieldElement& other);
+    friend FieldElement operator-(const FieldElement& other);
+    friend FieldElement operator*(const FieldElement& other);
+    // Note: Bitwise OR is commandeered for the power operator
+    friend FieldElement operator^(const int& power);
+    friend FieldElement operator/(const FieldElement& other);
     friend std::ostream& operator<<(std::ostream& os, const FieldElement& element);
     
     int Number;
     int Prime;
 };
-
-bool operator==(const FieldElement& lhs, const FieldElement& rhs)
-{
-    return lhs.Number == rhs.Number && lhs.Prime == rhs.Prime;
-}
-
-bool operator!=(const FieldElement& lhs, const FieldElement& rhs)
-{
-    return lhs.Number != rhs.Number || lhs.Prime != rhs.Prime;
-}
-
-std::ostream& operator<<(std::ostream& os, const FieldElement& element)
-{
-    // write obj to stream
-    os << "FieldElement_" << element.Prime << "(" << element.Number << ")";
-    return os;
-}
 
 }
--- bitcoin/src/Fields/src/FieldElement.cpp
+++ bitcoin/src/Fields/src/FieldElement.cpp
@@ -8,6 +8,7 @@
 
 #include "FieldElement.hpp"
 
+#include <cmath>
 #include <exception>
 
 namespace field
@@ -24,6 +25,65 @@ FieldElement::FieldElement(int number, int prime)
        error << "Number " << Number << " not in field range [0," << Prime-1 << "].";
        throw std::runtime_error(error.str());
    }
+}
+
+bool operator==(const FieldElement& lhs, const FieldElement& rhs)
+{
+    return lhs.Number == rhs.Number && lhs.Prime == rhs.Prime;
+}
+
+bool operator!=(const FieldElement& lhs, const FieldElement& rhs)
+{
+    return lhs.Number != rhs.Number || lhs.Prime != rhs.Prime;
+}
+
+FieldElement operator+(const FieldElement& other)
+{
+    // Elements must be in the same finite field, otherwise the calculation is meaningless
+    if (Prime != other.Prime)
+    {
+        throw std::runtime_error("Cannot add two numbers in different fields");
+    }
+
+    auto number = (Number + other.Number) % Prime;
+    return FieldElement(number, Prime);
+}
+
+FieldElement operator-(const FieldElement& other)
+{
+    // Elements must be in the same finite field, otherwise the calculation is meaningless
+    if (Prime != other.Prime)
+    {
+        throw std::runtime_error("Cannot substract two numbers in different fields");
+    }
+
+    auto number = (Number - other.Number) % Prime;
+    return FieldElement(number, Prime);
+}
+
+FieldElement operator*(const FieldElement& other)
+{
+    // Elements must be in the same finite field, otherwise the calculation is meaningless
+    if (Prime != other.Prime)
+    {
+        throw std::runtime_error("Cannot multiply two numbers in different fields");
+    }
+
+    auto number = (Number * other.Number) % Prime;
+    return FieldElement(number, Prime);
+}
+
+FieldElement operator^(const int& power)
+{
+    auto number = std::pow(Number, power) % Prime;
+    return FieldElement(number, Prime);
+}
+
+std::ostream& operator<<(std::ostream& os, const FieldElement& element)
+{
+    // write obj to stream
+    os << "FieldElement_" << element.Prime << "(" << element.Number << ")";
+    return os;
 }
 
 }
